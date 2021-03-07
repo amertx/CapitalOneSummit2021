4af4dae9a2b891d1826a42078c4a3976920000eb
amertx <amerdtx@gmail.com>
2021-03-06 23:28:36 -0600

    added new changes

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template, url_for
 import os
+import requests
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def hello():
-    return render_template('homePage.html')
+    url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/reference/v1.0/currencies"
+
+    headers = {
+        'x-rapidapi-key': "6bcc904facmshf03178edfc4450ap1e5c79jsn2d895f1c6d49",
+        'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+        }
+
+    response = requests.request("GET", url, headers=headers)
+
+    currencies = response.json()
+
+    return render_template('homePage.html', currencies=currencies)
+
+
+#input parameters
 
 if __name__ == '__main__':
     app.debug = True
